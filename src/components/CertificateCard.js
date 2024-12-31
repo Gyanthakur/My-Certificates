baceb7702521a741025fcfed5bd5415cb8303a06
Gyanthakur <gps.96169@gmail.com>
2024-12-31 18:30:04 +0530

    update: certificate card

--- src/components/CertificateCard.js
+++ src/components/CertificateCard.js
@@ -135,22 +135,22 @@ export default function CertificateCard({ certificate }) {
     //     )}
     //   </div>
     // </div>
-	<div className="relative p-4 rounded bg-gradient-to-t from-transparent via-cyan-200 to-transparent group">
+	<div className="relative p-4 border border-blue-600 rounded bg-gradient-to-t from-transparent via-cyan-200 to-transparent group">
   {/* Gradient border animations */}
 
   <div className="absolute inset-0 rounded overflow-hidden pointer-events-none">
-    <div className="absolute w-full h-[3px] bg-gradient-to-l from-transparent via-red-500 to-transparent animate-borderMove top-0"></div>
-    <div className="absolute w-full h-[3px] bg-gradient-to-l from-transparent via-red-500 to-transparent animate-borderMoveRtL bottom-0"></div>
-    <div className="absolute w-[3px] h-full bg-gradient-to-t from-transparent via-pink-500 to-transparent animate-borderMovey left-0"></div>
-    <div className="absolute w-[3px] h-full bg-gradient-to-t from-transparent via-pink-500 to-transparent animate-borderMoveydown right-0"></div>
+    <div className="absolute w-full h-[3px] bg-gradient-to-l from-transparent via-blue-500 to-transparent animate-borderMove top-0"></div>
+    <div className="absolute w-full h-[3px] bg-gradient-to-l from-transparent via-blue-500 to-transparent animate-borderMoveRtL bottom-0"></div>
+    <div className="absolute w-[3px] h-full bg-gradient-to-t from-transparent via-blue-500 to-transparent animate-borderMovey left-0"></div>
+    <div className="absolute w-[3px] h-full bg-gradient-to-t from-transparent via-blue-500 to-transparent animate-borderMoveydown right-0"></div>
   </div>
   {/* Content */}
-  <div className="relative bg-white p-6 rounded shadow-lg hover:shadow-2xl transition-shadow duration-300">
+  <div className="relative  bg-white p-6 rounded shadow-lg hover:shadow-2xl transition-shadow duration-300">
   <ToastContainer position="top-right" autoClose={3000} />
     <img
       src={certificate.image}
       alt={certificate.name}
-      className="w-full h-35 object-cover rounded transition-transform transform group-hover:scale-105 duration-300"
+      className="w-full h-25 object-cover rounded transition-transform transform group-hover:scale-105 duration-300"
     />
     <div className="mt-6">
       <h2 className="text-xl font-bold mb-2 text-green-700 group-hover:text-blue-600 transition-colors duration-300">
