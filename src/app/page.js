328d6a9747cc091df33b12fe5e08f669390c95fb
Gyanthakur <gps.96169@gmail.com>
2024-12-31 16:45:14 +0530

    update: home page

--- src/app/page.js
+++ src/app/page.js
@@ -16,8 +16,9 @@ export default function HomePage() {
   }, []);
 
   return (
-    <div className="container mx-auto">
-      <h1 className="text-3xl font-bold text-center my-8">Certificates</h1>
+    <div className="min-h-screen bg-gradient-to-r from-blue-100 via-indigo-100 to-purple-100 py-12 px-4 sm:px-6 lg:px-8">
+      <h1 className="text-3xl font-bold text-center my-8"></h1>
+      <h1 className="text-4xl font-bold text-center mb-6 text-blue-600">Certificates</h1>
       <div className="grid grid-cols-1 m-10 md:grid-cols-2 lg:grid-cols-3 gap-4">
         {certificates?.map((cert) => (
           <CertificateCard key={cert._id} certificate={cert} />
