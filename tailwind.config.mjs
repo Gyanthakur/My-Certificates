b4ea9a6dfd299ddbbc046cfa4727f951b14f8e69
Gyanthakur <gps.96169@gmail.com>
2024-12-31 12:31:27 +0530

    added tailwind animation

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -11,6 +11,36 @@ export default {
         background: "var(--background)",
         foreground: "var(--foreground)",
       },
+      keyframes: {
+        // Horizontal movement from left to right
+        borderMove: {
+          '0%': { transform: 'translateX(-100%)' },
+          '100%': { transform: 'translateX(100%)' },
+        },
+        // Horizontal movement from right to left
+        borderMoveRtL: {
+          '0%': { transform: 'translateX(100%)' },
+          '100%': { transform: 'translateX(-100%)' },
+        },
+        // Vertical movement from top to bottom
+        borderMovey: {
+          '0%': { transform: 'translateY(100%)' },
+          '100%': { transform: 'translateY(-100%)' },
+        },
+        // Vertical movement from bottom to top
+        borderMoveydown: {
+          '0%': { transform: 'translateY(-100%)' },
+          '100%': { transform: 'translateY(100%)' },
+        },
+      },
+      animation: {
+        // Horizontal movement animation
+        borderMove: 'borderMove 3s linear infinite',
+        borderMoveRtL: 'borderMoveRtL 3s linear infinite',
+        // Vertical movement animation
+        borderMovey: 'borderMovey 3s linear infinite',
+        borderMoveydown: 'borderMoveydown 3s linear infinite',
+      },
     },
   },
   plugins: [],
